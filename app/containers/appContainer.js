9e20d4e0aa26e674e7f6f44dc88ef0171b54ebd7
Максим Швецов <mshvetsov@at-consulting.ru>
2017-08-03 20:55:33 +0300

    Нарисовал карту

--- app/containers/appContainer.js
+++ app/containers/appContainer.js
@@ -13,10 +13,39 @@ class AppContainer extends Component {
     console.log('main container mounted');
   }
 
+  cardFlipped(event) {
+    if (event
+      && event.currentTarget
+      && event.currentTarget.className) {
+      if (event.currentTarget.className.indexOf('flipped') === -1) {
+        event.currentTarget.className = 'card flipped';
+      }
+      else {
+        event.currentTarget.className = 'card';
+      }
+    }
+  }
+
   render() {
 
     return (
-      <div>main container</div>
+      <div>
+        <div className="card" onClick={this.cardFlipped.bind(this)}>
+          <div className="card-face">
+            <div className="card-face__corner top-right">&nbsp;</div>
+            <div className="card-face__corner top-left">&nbsp;</div>
+            <div className="card-face__corner bottom-right">&nbsp;</div>
+            <div className="card-face__corner bottom-left">&nbsp;</div>
+            <div className="card-face__center">
+              <div className="comedy-icon">&nbsp;</div>
+            </div>
+            <div className="card-face__title">
+              Посмеяться
+            </div>
+          </div>
+          <div className="card-back">&nbsp;</div>
+        </div>
+      </div>
     );
   }
 }
